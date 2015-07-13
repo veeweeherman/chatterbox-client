586e4a80bf857dbbb76eb7a6ed5e51872b3dea09
TGA-Student <tga.student@hackreactor.com>
2015-07-13 15:20:00 -0700

    4 tests pass

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,25 +1,26 @@
 // YOUR CODE HERE:
 var app = {};
 app.init = function() {};
-app.send = function(URL, callback, data) {
+app.send = function(message) {
 	
 
 	$.ajax({
 	  // This is the url you should use to communicate with the parse API server.
-	  url: URL,
+	  // url: URL,
 	  type: 'POST',
-	  data: data,
-	  contentType: 'application/json',
-	  success: function (data) {
+	  // data: data;
+	 	data: JSON.stringify(message),
+	  // contentType: 'application/json',
+	  // success: function (data) {
     	
-	  	callback(data);
-    	console.log(message);
-  		// return data;
-  		},
-	  error: function (data) {
-	    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-	    console.error('chatterbox: Failed to send message');
-	  }
+	  // 	callback(data);
+   //  	console.log(message);
+  	// 	// return data;
+  	// 	},
+	  // error: function (data) {
+	  //   // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+	  //   console.error('chatterbox: Failed to send message');
+	  // }
 	});
 
 
