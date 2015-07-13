a024a2abed0f2adce8a595c8fcfaa0e496d0ee2d
TGA-Student <tga.student@hackreactor.com>
2015-07-13 15:24:14 -0700

    app beh' test finished

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -7,4 +7,9 @@ app.send = function(message) {
  		data: JSON.stringify(message),
 	});
 };
-app.fetch = function () {};
+app.fetch = function (data) {
+	$.ajax({
+		type: 'GET',
+		data: JSON.stringify(data),
+	});
+};
