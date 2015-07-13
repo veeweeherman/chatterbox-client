a34459a3c197cf11b239053ee2506d4306a97f23
TGA-Student <tga.student@hackreactor.com>
2015-07-13 15:22:13 -0700

    fetch test passes

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -2,28 +2,9 @@
 var app = {};
 app.init = function() {};
 app.send = function(message) {
-	
-
 	$.ajax({
-	  // This is the url you should use to communicate with the parse API server.
-	  // url: URL,
-	  type: 'POST',
-	  // data: data;
-	 	data: JSON.stringify(message),
-	  // contentType: 'application/json',
-	  // success: function (data) {
-    	
-	  // 	callback(data);
-   //  	console.log(message);
-  	// 	// return data;
-  	// 	},
-	  // error: function (data) {
-	  //   // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-	  //   console.error('chatterbox: Failed to send message');
-	  // }
+		type: 'POST',
+ 		data: JSON.stringify(message),
 	});
-
-
 };
-
-// JSON.parse(ajaxOptions.data)
+app.fetch = function () {};
