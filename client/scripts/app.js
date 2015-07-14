f18774040eaf937f6f0fb819597a61984d2a009c
TGA-Student <tga.student@hackreactor.com>
2015-07-14 14:24:48 -0700

    init function works

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,71 +1,75 @@
 // YOUR CODE HERE:
-var app = {};
+var app = {
 
-app.init = function(){
-	$(document).ready = (function() {
-		app.fetch();
-		// app.addFriend();
-		// app.addMessage();
-	});
+	init: function(){
+		$(document).ready(function() {
+			setInterval(app.fetch, 1000);
+			$('#submitMessage').on("click", function() {console.log('oh HAY clicking')});
+			  app.addFriend();
+			app.addMessage();
+		});
+	},
+
+	send: function(message) {
+		$.ajax({
+		  url: 'https://api.parse.com/1/classes/chatterbox',
+		  type: 'POST',
+		  data: JSON.stringify(message),
+		  contentType: 'application/json',
+		  success: function (data) {
+		    console.log('Chatterbox: Message sent');
+		  },
+		  error: function (data) {
+		    console.error('Chatterbox: Failed to send message');
+		  }
+		});
+
+	},
+
+
+	fetch: function() {
+		$.ajax({
+			url: 'https://api.parse.com/1/classes/chatterbox',
+			type: 'GET',
+			contentType: 'application/json',
+			success: function(data) {
+		  	for (var i = 0; i < data.results.length; i++) {
+		  			$('#main').append('<div>' + _.escape(data.results[i].username) + '<br>' + _.escape(data.results[i].text) + '</div>');
+		  	};
+		  },
+			error: function() {
+			  console.error('Chatterbox: Failed to send message');
+			}
+		});
+	},
+
+
+
+	clearMessages: function() {
+	  	$('#chats').empty();
+	},
+
+	addMessage: function(message) {
+		$('#chats').append("<p>"+message+"</p>");
+	},
+
+	addRoom: function(child) {
+		$('#roomSelect').append("<div></div>")
+	},
+
+	addFriend: function(){
+		$('.username').on('click', function(){
+		});
+	},
+
+ 	server: 'https://api.parse.com/1/classes/chatterbox'
 };
-// setInterval(app.fetch, 1000);
-// app.init();
-// server: 'https://api.parse.com/1/classes/chatterbox';
-app.send = function(message) {
-	$.ajax({
-	  url: 'https://api.parse.com/1/classes/chatterbox',
-	  type: 'POST',
-	  data: JSON.stringify(message),
-	  contentType: 'application/json',
-	  success: function (data) {
-	    console.log('chatterbox: Message sent');
-	  },
-	  error: function (data) {
-	    console.error('chatterbox: Failed to send message');
-	  }
-	});
+app.init();
 
-};
-
-var userObject = {
-	username: null,
-	text: null,
-	roomname: null
-};
-
-app.fetch = function() {
-	$.ajax({
-		url: 'https://api.parse.com/1/classes/chatterbox',
-		type: 'GET',
-		contentType: 'application/json',
-		success: function(data) {
-	  	for (var i = 0; i < data.results.length; i++) {
-	  			$('#main').append('<div>' + _.escape(data.results[i].username) + '<br>' + _.escape(data.results[i].text) + '</div>');
-	  	};
-	  },
-		error: function() {
-		  console.error('chatterbox: Failed to send message');
-		}
-	});
-};
-
-// setInterval(app.fetch, 1000)
-
-app.clearMessages = function() {
-  	$('#chats').empty();
-};
-
-app.addMessage = function(message) {
-	$('#chats').append("<p>"+message.text+"</p>");
-}
-
-app.addRoom = function(child) {
-	$('#roomSelect').append("<div></div>")
-}
-
-app.addFriend = function(){
-	$('.username').on('click', function(){
-	});
-}
-
-
+// var message = prompt("Whats your message?");
+// $('body').append('message');
+	// var userObject = {
+	// 	username: null,
+	// 	text: null,
+	// 	roomname: null
+	// };
