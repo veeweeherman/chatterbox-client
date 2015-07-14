8e8554099045c113eaa21dd0bfe6d0fcc6a75ea7
TGA-Student <tga.student@hackreactor.com>
2015-07-13 17:23:09 -0700

    setting up keys and parse ID

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -4,23 +4,45 @@ app.init = function() {};
 app.send = function(message) {
 	$.ajax({
 		type: 'POST',
- 		data: JSON.stringify(message),
+ 		url: 'https://api.parse.com/1/classes/chatterbox',
+ 		data: JSON.stringify(message)
 	});
 };
-app.fetch = function (data) {
+
+app.fetch = function () {
 	$.ajax({
 		type: 'GET',
-		data: JSON.stringify(data),
+		url: 'https://api.parse.com/1/classes/chatterbox',
+		data: JSON.stringify(data)
 	});
 };
+
 app.clearMessages = function() {
   	$('#chats').empty();
 };
+
 app.addMessage = function(message) {
 	$('#chats').append("<p>"+message.text+"</p>");
-	console.log($('#chats'))
+	// console.log($('#chats'))
 }
+
 app.addRoom = function(child) {
 	$('#roomSelect').append("<div></div>")
-	console.log($('#roomSelect')); //.append(room);
+	// console.log($('#roomSelect')); //.append(room);
 }
+
+app.addFriend = function(){
+	// app.init();
+
+	$('.username').on('click', function(){
+		// $('#chats').append();
+			$('#main').append("<p class='username'>"+message.username+"</p>")
+	});
+
+	// console.log($('#main'));
+}
+app.fetch();
+
+
+
+console.log($('#main'));
