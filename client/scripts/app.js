b5fc8e9545fd3f6c902e3b809b510c9510a33b0f
TGA-Student <tga.student@hackreactor.com>
2015-07-14 11:21:58 -0700

    button works to refresh chats

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,9 +1,18 @@
 // YOUR CODE HERE:
 var app = {};
-app.init = function(){};
+
+app.init = function(){
+	$(document).ready = (function() {
+		app.fetch();
+		// app.addFriend();
+		// app.addMessage();
+	});
+};
+// setInterval(app.fetch, 1000);
+// app.init();
+// server: 'https://api.parse.com/1/classes/chatterbox';
 app.send = function(message) {
 	$.ajax({
-	  // This is the url you should use to communicate with the parse API server.
 	  url: 'https://api.parse.com/1/classes/chatterbox',
 	  type: 'POST',
 	  data: JSON.stringify(message),
@@ -12,63 +21,43 @@ app.send = function(message) {
 	    console.log('chatterbox: Message sent');
 	  },
 	  error: function (data) {
-	    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
 	    console.error('chatterbox: Failed to send message');
 	  }
 	});
 };
-window.allInfo = {};
-app.fetch = function () {
+
+app.fetch = function() {
 	$.ajax({
 		url: 'https://api.parse.com/1/classes/chatterbox',
 		type: 'GET',
 		contentType: 'application/json',
-		success: function (data) {
-		  
+		success: function(data) {
 	  	for (var i = 0; i < data.results.length; i++) {
-	  			$('body').append(data.results[i].username);
+	  			$('#main').append('<div>' + _.escape(data.results[i].username) + '<br>' + _.escape(data.results[i].text) + '</div>');
 	  	};
-	  	// for (var i = 0; i < data.results.length; i++) {
-  		// 	allInfo.data[results[i][username]] = data.results[i][username];
-	  	// }
 	  },
-		error: function () {
-		  // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+		error: function() {
 		  console.error('chatterbox: Failed to send message');
-		},
-		// data: JSON.parse(message)
-
+		}
 	});
 };
-app.fetch();
-// setInterval(app.fetch, 10000)
-console.log(allInfo)
+
+// setInterval(app.fetch, 1000)
+
 app.clearMessages = function() {
   	$('#chats').empty();
 };
 
 app.addMessage = function(message) {
 	$('#chats').append("<p>"+message.text+"</p>");
-	// console.log($('#chats'))
 }
 
 app.addRoom = function(child) {
 	$('#roomSelect').append("<div></div>")
-	// console.log($('#roomSelect')); //.append(room);
 }
 
 app.addFriend = function(){
-	// app.init();
-
 	$('.username').on('click', function(){
-		// $('#chats').append();
-			// $('#main').append("<p class='username'>"+message.username+"</p>")
 	});
-
-	// console.log($('#main'));
 }
-// app.fetch();
 
-
-
-console.log($('#main').find('.username'));
